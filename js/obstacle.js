d0d12b20bcaaa1292d51bb43f9d669822a5949c5
burak algan <burakalgan@gmail.com>
2023-11-10 22:16:16 +0100

    create images folder and js folder

---
+++ js/obstacle.js
@@ -0,0 +1,28 @@
+class Obstacle {
+  constructor(gameScreen) {
+    this.gameScreen = gameScreen;
+    this.left = Math.floor(Math.random() * 300 + 70);
+    this.top = 0;
+    this.width = 100;
+    this.height = 150;
+    this.element = document.createElement("img");
+
+    this.element.src = "./images/santa.png";
+    this.element.style.position = "absolute";
+    this.element.style.width = `${this.width}px`;
+    this.element.style.height = `${this.height}px`;
+    this.element.style.top = `${this.top}px`;
+    this.element.style.left = `${this.left}px`;
+    this.gameScreen.appendChild(this.element);
+  }
+  updatePosition() {
+    this.element.style.left = `${this.left}px`;
+    this.element.style.top = `${this.top}px`;
+    console.log("obstacle position", this.element.getBoundingClientRect());
+  }
+
+  move() {
+    this.top += 3;
+    this.updatePosition();
+  }
+}
